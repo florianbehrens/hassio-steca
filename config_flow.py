9f7167f098acee0d2c431d562e1e3ae8c68c4963
Florian Behrens <florianbehrens@users.noreply.github.com>
2022-10-27 20:08:04 +0200

    More config flow.

--- config_flow.py
+++ config_flow.py
@@ -1,13 +1,28 @@
 from homeassistant import config_entries
-from .const import DOMAIN
+from voluptuous import Schema, Required
 
-import voluptuous as vol
 
-class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    async def async_step_user(self, info):
-        if info is not None:
-            pass  # TODO: process info
+class ExampleConfigFlow(config_entries.ConfigFlow, domain="steca"):
+    """Handle a Steca config flow."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            # Validate user input
+            valid = True
+            if valid:
+                # See next section on create entry usage
+                return self.async_create_entry(
+                            title="Steca coolcept inverter IP address",
+                            data={
+                                "ip": user_input["ip"]
+                            },
+                        )
+
+            errors["base"] = "auth_error"
 
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({vol.Required("password"): str})
+            step_id="user", data_schema=Schema({Required('ip'): str})
         )
--- sensor.py
+++ sensor.py
@@ -45,6 +45,16 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 _LOGGER = logging.getLogger(__name__)
 
+async def async_setup_entry(hass, config_entry, async_add_devices):
+    """Set up entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(
+            config_entry, "sensor"
+        )
+    )
+
+    return True
+
 
 async def async_setup_platform(
     hass: HomeAssistant,
